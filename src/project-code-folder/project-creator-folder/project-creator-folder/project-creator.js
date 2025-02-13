772adedeba30fcd5498c5acc53e0d275617c308a
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2025-02-12 21:14:49 -0500

    added new project-creator comments

--- src/project-code-folder/project-creator-folder/project-creator-folder/project-creator.js
+++ src/project-code-folder/project-creator-folder/project-creator-folder/project-creator.js
@@ -4,6 +4,7 @@ export {createAndAppendProject}
 
 let projectIdCount = 0;
 
+//Creating Project
     function createProject(h1Text,pText){
         let h1TextObj = h1Text;
         let pTextObj = pText;
@@ -14,15 +15,18 @@ let projectIdCount = 0;
         return{h1TextObj,pTextObj,projectId,todoIdCounter,todoArray}
     }
 
+    //sets the projects Id and updates the projectIdCount 
     function setAndIncreaseProjectIdCount(project){
         project.projectId = projectIdCount;
         projectIdCount++;
     }
 
+    //Appends the newly created project to the array
     function appendProjectToArray(project,projectDisplayArray){
         projectDisplayArray.push(project);
     }
 
+    // Wrapper function that both creates and appends the project to the selected Array
     function createAndAppendProject(h1,p,projectDisplayArray){
         let tempProject = createProject(h1,p);
         setAndIncreaseProjectIdCount(tempProject);
