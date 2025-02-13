c2dc9320efec6048a92e76d8a1cd474a8e2f46ec
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2025-02-12 20:53:35 -0500

    have basic functions of creating a todo (nothing permanent just testing)

--- src/todo-code-folder/todo-creator-folder/todo-creator.js
+++ src/todo-code-folder/todo-creator-folder/todo-creator.js
@@ -1,6 +1,28 @@
+//Creating Todo
 function createTodo(id,description){
     let todoId = id;
     let todoDescription = description; 
     let checkedStatus = false;
     return {todoId,todoDescription,checkedStatus}
 }
+
+//Find the index of the projects id 
+function findIndex(array,pId){
+    function getUniqueId(object) { 
+        return object.projectId === pId;
+      }
+    
+    const selectedIndex = array.findIndex(getUniqueId);
+    return selectedIndex
+}
+
+//Appending Todo To Array
+function appendTodoToArray(array,projectId,todo){
+    let projectIndex = findIndex(array,projectId)
+    array[projectIndex].todoArray.push(todo)
+}
+
+//Wrapper function that both creates and appends the todo to the selected projects todoArray
+// function createAndAppendProject(){
+
+// }
