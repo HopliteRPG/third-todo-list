65f7f52d1fecf99935197e2417436fc849f65d12
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2025-02-12 21:11:12 -0500

    updated todo comments for easier reading

--- src/todo-code-folder/todo-creator-folder/todo-creator.js
+++ src/todo-code-folder/todo-creator-folder/todo-creator.js
@@ -10,30 +10,33 @@ function findIndex(array,pId){
     return selectedIndex
 }
 
-//Creating Todo
-function createTodo(description){
-    let todoId = undefined;
-    let todoDescription = description; 
-    let checkedStatus = false;
-    return {todoId,todoDescription,checkedStatus}
-}
+//Todo Creation
 
-//appends the todo Id and updates the todoIdCounter for the selected project
-function appendTodoIdAndUpdateTodoIdCounter(array,projectId,todo){
-    let projectIndex = findIndex(array,projectId)
-    todo.todoId = array[projectIndex].todoIdCounter;
-    array[projectIndex].todoIdCounter++
-  }
+    //Creating Todo
+    function createTodo(description){
+        let todoId = undefined;
+        let todoDescription = description; 
+        let checkedStatus = false;
+        return {todoId,todoDescription,checkedStatus}
+    }
 
-  //Appending Todo To Array
-function appendTodoToArray(array,projectId,todo){
-    let projectIndex = findIndex(array,projectId)
-    array[projectIndex].todoArray.push(todo)
-}
+    //appends the todo Id and updates the todoIdCounter for the selected project
+    function appendTodoIdAndUpdateTodoIdCounter(array,projectId,todo){
+        let projectIndex = findIndex(array,projectId)
+        todo.todoId = array[projectIndex].todoIdCounter;
+        array[projectIndex].todoIdCounter++
+    }
 
-// Wrapper function that both creates and appends the todo to the selected projects todoArray
-function createAndAppendTodo(description,array,projectId){
-    let tempTodo = createTodo(description);
-    appendTodoIdAndUpdateTodoIdCounter(array,projectId,tempTodo);
-    appendTodoToArray(array,projectId,tempTodo);
-}
+    //Appending Todo To Array
+    function appendTodoToArray(array,projectId,todo){
+        let projectIndex = findIndex(array,projectId)
+        array[projectIndex].todoArray.push(todo)
+    }
+
+    // Wrapper function that both creates and appends the todo to the selected projects todoArray
+    function createAndAppendTodo(description,array,projectId){
+        let tempTodo = createTodo(description);
+        appendTodoIdAndUpdateTodoIdCounter(array,projectId,tempTodo);
+        appendTodoToArray(array,projectId,tempTodo);
+    }
+
