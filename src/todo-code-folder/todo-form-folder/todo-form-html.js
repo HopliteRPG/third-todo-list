3dd124692e47f0952f788a1f0d56cdaa00e3280a
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2025-03-09 22:40:23 -0400

    Have the basic form in a function

--- src/todo-code-folder/todo-form-folder/todo-form-html.js
+++ src/todo-code-folder/todo-form-folder/todo-form-html.js
@@ -1,5 +1,6 @@
 const dialogCreate = document.createElement("dialog");
 const formCreate = document.createElement("form");
+const labelCreate = document.createElement("label");
 const divCreate = document.createElement("div");
 const h1Create = document.createElement("h1");
 const pCreate = document.createElement("p");
@@ -10,6 +11,37 @@ const todoDialogHolder = document.querySelector(".todoDialogHolder")
 
 
 //Creating Html
+function createDialog(parentDiv,dialog,className){
+    let cloneDialogCreate = dialog.cloneNode(true);
+    cloneDialogCreate.classList.add(className)
+    parentDiv.appendChild(cloneDialogCreate);
+    return{cloneDialogCreate}
+}
+
+function createForm(parentDiv,form,className){
+    let cloneFormCreate = form.cloneNode(true);
+    cloneFormCreate.classList.add(className)
+    parentDiv.appendChild(cloneFormCreate);
+    return{cloneFormCreate}
+}
+
+function createLabel(parentDiv,label,id){
+    let cloneLabelCreate = label.cloneNode(true);
+    cloneLabelCreate.setAttribute("id",id);
+    parentDiv.appendChild(cloneLabelCreate);
+    return{cloneLabelCreate}
+}
+
+function createInput(parentDiv,input,inputName,type,maxLengthNum,reqOrNot){
+    let cloneInputCreate = input.cloneNode(true);
+    cloneInputCreate.setAttribute("name",inputName);
+    cloneInputCreate.setAttribute("type",type);
+    cloneInputCreate.setAttribute("maxlength",maxLengthNum);
+    cloneInputCreate.setAttribute("required",reqOrNot)
+    parentDiv.appendChild(cloneInputCreate);
+
+}
+
 function createDivSection(parentDiv,div,className){
     let cloneDivCreate = div.cloneNode(true);
     cloneDivCreate.classList.add(className)
@@ -44,5 +76,15 @@ function createDivSection(parentDiv,div,className){
 
 
 function createTodoForm(){
-todoDialogHolder.appendChild(dialogCreate)
+let todoDialog = createDialog(todoDialogHolder,dialogCreate,"todoDialog");
+let todoForm = createForm(todoDialog,form,"todoForm");
+let formDiv = createDivSection(todoForm,divCreate,"form-div");
+let formTodoPDiv = createDivSection(formDiv,createDivSection,"form-todo-p-div")
+let todoPLabel = createLabel(formTodoPDiv,labelCreate,"todo_p");
+let todoPInput = createInput(formTodoPDiv,inputCreate,todo_p,"text",15,true);
+
+let todoSubmitButton = createButton(formDiv,buttonCreate,"Submit","todo-submit-btn");
+todoSubmitButton.setAttribute("type","submit");
+
+let todoCloseButton = createButton(formDiv,buttonCreate,"Close","todo-close-btn")
 }
